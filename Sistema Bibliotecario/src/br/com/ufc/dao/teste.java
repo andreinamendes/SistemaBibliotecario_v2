1246d116390a29b82f7daa137d55e22d531a2de5
Andreina Mendes <andreinamendes63@alu.ufc.br>
2019-11-12 14:34:31 -0300

    add packages

--- Sistema Bibliotecario/src/br/com/ufc/dao/teste.java
+++
@@ -1,7 +0,0 @@
-package br.com.ufc.dao;
-
-public class teste {
-	public static void main(String[] args) {
-		System.out.println("teste");
-	}
-}
---
+++ Sistema Bibliotecario/src/br/com/ufc/view/Main.java
@@ -0,0 +1,5 @@
+package br.com.ufc.view;
+
+public class Main {
+
+}
