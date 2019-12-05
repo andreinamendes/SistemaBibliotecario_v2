470c753ef6b46b0e98e86381106ad59cdaaae1a4
Andreina Mendes <andreinamendes63@alu.ufc.br>
2019-12-04 23:31:38 -0300

    Update

--- Sistema Bibliotecario/libs/population.sql
+++ Sistema Bibliotecario/libs/population.sql
@@ -31,3 +31,28 @@ INSERT INTO unidade VALUES(8273, 1029);
 INSERT INTO unidade VALUES(8732, 1567);
 INSERT INTO unidade VALUES(4823, 1097);
 INSERT INTO unidade VALUES(3827, 1239);
+
+--POPULATION USUÁRIOS
+
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Maria Mendonca','1234','maria@gmail.com','12345678910','1987-06-28','Rua Pinto Damasceno','2255','Caninde','Ceara')
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Gregorio Neto','1234','gregorio@gmail.com','12345678911','1999-05-17','Avenida Bosques de Cabral','124','Crato','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Maria Emilia','1234','emilia@gmail.com','12345678912','2000-04-03','Rua Joao Pinto','36','Reboussas','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Ricado Alberto','1234','ricardo@gmail.com','12345678913','2001-05-06','Rua Aroldo Lopez','1258A','Sobral','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Roger Maciel','1234','roger@gmail.com','12345678914','1995-07-09','Rua Lucas Rosse','965','Quixada','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Gabriel Medeiros','1234','gabriel@gmail.com','12345678915','1976-02-10','Rua Sao Braz','645','Impueiras','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Galena Gonsalez','1234','galena@gmail.com','12345678916','1994-03-20','Rua Milho Assado','621','Fortaleza','Ceara');
+INSERT INTO usuario (nome, senha, email, cpf, data_nasc, rua, numero, cidade, estado) VALUES ('Constantino Lopes','1234','constantino@gmail.com','12345678917','1993-09-27','Rua Mao de Vaca','879','Caridade','Ceara');
+
+--POPULATION ALUNOS
+
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (001, 2, 'Engenharia de Computacao');
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (002, 3, 'Engenharia de Software');
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (003, 4, 'Ciencia da Computacao');
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (004, 5, 'Design Digital');
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (005, 7, 'Redes de Computadores');
+INSERT INTO aluno (matricula, id_usr, curso) VALUES (006, 8, 'Sistemas de Informacao');
+
+--POPULATION SERVIDORES
+INSERT INTO servidor (siape, id_usr, nivel_acc) VALUES (001, 1, 3);
+INSERT INTO servidor (siape, id_usr, nivel_acc) VALUES (002, 6, 3);
+
